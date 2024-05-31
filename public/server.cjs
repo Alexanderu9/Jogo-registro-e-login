40ad72f40e885a41b739387a827a5837cfc77afa
Alexander Urdaneta <alexanderu7.aug@gmail.com>
2024-05-31 19:00:12 -0300

    Update server.cjs

--- public/server.cjs
+++ public/server.cjs
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sirviendo archivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/public')));
 
 
 
